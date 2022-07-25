e86cbbf14bc67cc05c4b9f85070abc3730478f1d
Bruno Meira <bruno.meiramelhado@gmail.com>
2022-07-25 09:47:10 -0300

    git init and configure users

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,16 @@
+class ApplicationController < ActionController::Base
+
+  # protected
+
+  # def after_sign_in_path_for(resource)
+  #   root_path
+  # end
+
+  # def after_sign_out_path_for(resource)
+  #   root_path
+  # end
+
+  # def after_sign_up_path_for(resource)
+  #   root_path
+  # end
+end
